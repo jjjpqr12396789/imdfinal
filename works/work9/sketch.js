47ffd693c0e20b245953c9aa643bd408763f40ea
05282828yw <78583485+jjjpqr12396789@users.noreply.github.com>
2022-12-23 12:13:59 +0900

    affga

---
+++ works/work9/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  let boundingRects = document
+    .getElementById("p5Canvas")
+    .getBoundingClientRect();
+  let canvas = createCanvas(boundingRects.width, boundingRects.height);
+  canvas.parent("p5Canvas");
+  strokeWeight(2);
+  noFill();
+}
+
+function draw() {
+  if (mouseIsPressed && mouseButton == LEFT) {
+    stroke(random(256), random(256), random(256));
+    push();
+    translate(width / 2, height / 2);
+
+    var circleResolution = int(map(mouseY + 100, 0, height, 2, 10));
+    var radius = mouseX - width / 2;
+    var angle = TAU / circleResolution;
+
+    beginShape();
+    for (var i = 0; i <= circleResolution; i++) {
+      var x = cos(angle * i) * radius;
+      var y = sin(angle * i) * radius;
+      vertex(x, y);
+    }
+    endShape();
+
+    pop();
+  }
+}
